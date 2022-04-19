cd18e93e0c55563e6ae2a63ecd3c08ed7f152e19
phvleite <phvleite@gmail.com>
2022-04-19 19:41:03 -0300

    criado os seletores do requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,6 @@ function Home() {
     loading,
     filterByName,
     getFilterByName,
-    dbFilterByName,
   } = useContext(StarWarsContext);
 
   useEffect(() => {
@@ -16,27 +15,81 @@ function Home() {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    console.log(filterByName);
-  }, [filterByName]);
+  const selectedColumn = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
 
-  useEffect(() => {
-    console.log(dbFilterByName);
-  }, [dbFilterByName]);
+  const selectedOperator = [
+    'maior que',
+    'menor que',
+    'igual a',
+  ];
 
   return (
     <main>
-      <label htmlFor="input-filter-name">
-        Projeto Star Wars - Trybe
-        <input
-          id="input-filter-name"
-          data-testid="name-filter"
-          type="text"
-          value={ filterByName }
-          onChange={ getFilterByName }
-          placeholder="filtro por nome do planeta"
-        />
-      </label>
+      <section className="box-filter-name">
+        <label htmlFor="input-filter-name">
+          Projeto Star Wars - Trybe
+          <input
+            id="input-filter-name"
+            data-testid="name-filter"
+            type="text"
+            value={ filterByName }
+            onChange={ getFilterByName }
+            placeholder="filtro por nome do planeta"
+          />
+        </label>
+      </section>
+      <section className="box-filter-numerics">
+        <label htmlFor="column-filter">
+          Coluna
+          <select
+            id="column-filter"
+            data-testid="column-filter"
+            // value={ filterByName }
+            // onChange={ getFilterByName }
+          >
+            {selectedColumn.map((select) => (
+              <option key={ select } value={ select }>{select}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="comparison-filter">
+          Operador
+          <select
+            id="comparison-filter"
+            data-testid="comparison-filter"
+            // value={ filterByName }
+            // onChange={ getFilterByName }
+          >
+            {selectedOperator.map((select) => (
+              <option key={ select } value={ select }>{select}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="value-filter">
+          Valor
+          <input
+            id="value-filter"
+            data-testid="value-filter"
+            type="number"
+            // value={ filterByName }
+            // onChange={ getFilterByName }
+          />
+        </label>
+        <button
+          data-testid="button-filter"
+          className="button-filter"
+          type="button"
+          // onClick={ this.deleteExpense }
+        >
+          Filtrar
+        </button>
+      </section>
       { loading ? (<h1>Carregando...</h1>) : (
         <TableStarWars />
       )}
